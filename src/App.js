b7862b4d23d7e9b502332c2818eeaab708e05e8b
kaladin9017 <sanchochris@gmail.com>
2017-01-21 14:40:53 -0500

    added redux

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import SampleComponent from "./SampleComponent";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>C4Q Hackathon</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SampleComponent />
       </div>
     );
   }
