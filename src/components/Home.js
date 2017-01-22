83c683526a0b7208ce1575d3aa62c48a089785ce
Samantha Bretous <sam@samanthabretous.com>
2017-01-22 11:59:46 -0500

    start animation timeline

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,31 +20,30 @@ const renderBoy = ({target}) =>{
     .to(boy, 2, {backgroundPosition: "-1280px 0px", ease:SteppedEase.config(8), repeat:-1, repeatDelay:-.5 })
 }
 
-const renderWelcome = ({target}) =>{
+const hideWelcome = ({target}) =>{
   const welcome = target.find({className: 'welcome'})
-  return new TweenMax
-    .to(welcome, 2, {backgroundPosition: "-1280px 0px", ease:SteppedEase.config(8), repeat:-1, repeatDelay:-.5 })
+  const boy = target.find({className: 'spirte'})
+  const box = target.find({className: 'box'})
+  const buildings = target.find({className: 'buildings'})
+  const sky = target.find({className: 'sky'})
+  return new TimelineMax()
+    // .to(box, 1.2, {scale: .2, opacity: 0, ease: Power0.easeOut})
+    .pause()
+    .add('close')
+    .to(welcome, 1.2, {scale: .2, opacity: 0, ease: Power0.easeOut}, "close")
+    .to(boy, 2, {backgroundPosition: "-1280px 0px", ease:SteppedEase.config(8), repeat:-1, repeatDelay:-.5 }, "close")
+    .to(buildings, 1200, {backgroundPositionX: 3599, ease: Power0.easeOut }, "close")
 }
 
 class Home extends React.Component {
   componentDidMount() {
     this.clouds = this.addAnimation(renderClouds)
-    this.buildings = this.addAnimation(renderBuildings)
-    this.boy = this.addAnimation(renderBoy)
-    this.welcome = this.addAnimation(renderWelcome)
-    this.buildings.pause();
-    this.boy.pause();
-    this.welcome.pause();
+    this.welcome = this.addAnimation(hideWelcome)
   }
 
   //   componentDidUpdate() {
   //    this.switchAnim.tweenTo(this.getCurrentPage())
   // }
-  playNext(){
-    console.log(this.buildings)
-    this.buildings.play()
-    this.boy.play()
-  }
 
   render(){
     return (
@@ -52,8 +51,9 @@ class Home extends React.Component {
         <div className="sky"></div>
         <div className="buildings"></div>
         <div className="spirte"></div>
+        <div className="box" style={{width: 200, height:100, background:"red", position:"absolute", left: 500}}></div>
         <div className="welcome">
-          <button onClick={this.playNext.bind(this)}></button>
+          <button onClick={()=> this.welcome.tweenTo('close')}></button>
         </div>
       </div>
     )
